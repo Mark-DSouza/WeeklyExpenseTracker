cde9f4276d47fbdd706d375be82a6252bf9774f7
Mark-DSouza <markdsouza434@gmail.com>
2021-04-05 20:34:51 +0530

    Add fixes to user input Card

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -7,38 +7,52 @@ class NewTransaction extends StatelessWidget {
   final Function addTx;
   NewTransaction(this.addTx);
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Title',
-                    ),
-                    controller: titleController,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Amount',
-                    ),
-                    controller: amountController,
-                    keyboardType: TextInputType.numberWithOptions(decimal: true),
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      addTx(titleController.text, double.parse(amountController.text),);
-                    },
-                    child: Text('Add Transaction'),
-                    textColor: Colors.purple,
-                  ),
-                ],
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Title',
               ),
+              controller: titleController,
+              onSubmitted: (_) => submitData(),
             ),
-          );
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Amount',
+              ),
+              controller: amountController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => submitData(),
+            ),
+            FlatButton(
+              onPressed: submitData,
+              child: Text('Add Transaction'),
+              textColor: Colors.purple,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
