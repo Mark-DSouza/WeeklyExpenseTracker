e9d8d038c83f1b40ba47cc2a6d6a56c36919e561
Mark-DSouza <markdsouza434@gmail.com>
2021-04-06 17:21:34 +0530

    Refactor Transaction List to use ListTile as list items

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -32,47 +32,31 @@ class TransactionList extends StatelessWidget {
               ],
             )
           : ListView.builder(
-              itemBuilder: (context, index) {
+              itemBuilder: (contxt, index) {
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
-                          ),
-                        ),
-                        child: Text(
-                          '\$${transactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text(
+                            '\$${transactions[index].amount}',
                           ),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            transactions[index].title,
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(transactions[index].date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                        DateFormat.yMMMd().format(transactions[index].date)),
                   ),
                 );
               },
@@ -81,3 +65,46 @@ class TransactionList extends StatelessWidget {
     );
   }
 }
+
+// Card(
+//   child: Row(
+//     children: [
+//       Container(
+//         padding: EdgeInsets.all(10),
+//         margin: EdgeInsets.symmetric(
+//           vertical: 10,
+//           horizontal: 15,
+//         ),
+//         decoration: BoxDecoration(
+//           border: Border.all(
+//             color: Theme.of(context).primaryColor,
+//             width: 2,
+//           ),
+//         ),
+//         child: Text(
+//           '\$${transactions[index].amount.toStringAsFixed(2)}',
+//           style: TextStyle(
+//             color: Theme.of(context).primaryColor,
+//             fontWeight: FontWeight.bold,
+//             fontSize: 20,
+//           ),
+//         ),
+//       ),
+//       Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           Text(
+//             transactions[index].title,
+//             style: Theme.of(context).textTheme.headline6,
+//           ),
+//           Text(
+//             DateFormat.yMMMd().format(transactions[index].date),
+//             style: TextStyle(
+//               color: Colors.grey,
+//             ),
+//           ),
+//         ],
+//       ),
+//     ],
+//   ),
+// )
